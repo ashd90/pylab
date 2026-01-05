64e85c0019d8fbbdaea88e5e2f9305f09fcd0c42
Ashish Deshpande <232292705+ashd90@users.noreply.github.com>
2026-01-05 18:15:48 +0530

    added mini project

---
+++ 01-variables-and-strings/project-01.py
@@ -0,0 +1,11 @@
+# --- PROJECT: 1
+
+# 1. Ask the user for the text they want to "Purple-ify"
+year = int(input("Year you started learning python?  "))
+
+# 2. Logic: 
+
+total_years = 2025 - year
+print("-" * 30)
+print(f"Learning since {year} - {total_years} years of progress")
+print("-" * 30)
